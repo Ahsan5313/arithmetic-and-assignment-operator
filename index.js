d6990408891307beb79415fa9245f52ac395bd4d
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-04 14:49:37 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,18 @@
+arithmetic operator +  -  * / %(modulus) **(exponent that means we creat power by exponent ) ++ ,--;
+assignment operator =, += ,-=, *=, /=, %=, **=,;
+var a=9;
+a *=8;
+a=a*8;
+document.write(a);
+var a=9;
+a+=4;
+
+console.log(a);
+
+var a=20;
+a-=10;
+console.log(a);
+var x=52;
+x/=4;
+console.log(x);
+
